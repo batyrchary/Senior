bdc7c105ce71ae27070684c418772900f5937dc8
batyrchary <batyrchary95@gmail.com>
2018-03-10 21:51:45 -0800

    Update Question5.java

--- ceng495_CloudComputing/hw4/Question5.java
+++ ceng495_CloudComputing/hw4/Question5.java
@@ -11,38 +11,34 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
-
-
 import java.io.IOException;
 import java.util.StringTokenizer;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.mapreduce.tools.CLI;
+import org.apache.hadoop.mapred.JobConf;
 
 
 
 
-public class Question5 extends Configured implements Tool 
+//Case sensitivity was not checked -> if needed just add word.set(token.toLowerCase()); inside of if statement
+public class Question5 
 {
 	
     public static class Mapper1 extends Mapper<LongWritable, Text, Text, IntWritable> 
     {
 
-        private final static IntWritable one = new IntWritable(1);
-       // public Text word = new Text();
-
         @Override
         public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException 
         {
-           // String line = value.toString();
             StringTokenizer tokenizer = new StringTokenizer(value.toString());
             while (tokenizer.hasMoreTokens()) 
             {         
                 String token = tokenizer.nextToken();   
                 if(token.startsWith("#"))
                 {
-                    context.write(new Text(token),one);
-                   // word.set(token.toLowerCase());
-                   // context.write(word, one);
+                    context.write(new Text(token),new IntWritable(1));
                 }
             }
         }
@@ -53,7 +49,6 @@ public class Question5 extends Configured implements Tool
         @Override
         public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException 
         {    
-           // String line = value.toString();
             StringTokenizer tokenizer = new StringTokenizer(value.toString());
             while (tokenizer.hasMoreTokens()) 
             {       
@@ -69,7 +64,6 @@ public class Question5 extends Configured implements Tool
         @Override
         public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException 
         {    
-           // String line = value.toString();
             StringTokenizer tokenizer = new StringTokenizer(value.toString());
             while (tokenizer.hasMoreTokens()) 
             {    
@@ -83,13 +77,14 @@ public class Question5 extends Configured implements Tool
         {
             setup(context);
 
-            int rows = 0;
+            int counter = 0;
             while (context.nextKeyValue()) 
             {
                 
-                if (rows++ == 100)
+                if (counter == 100)
                     break;
-            
+                counter++;
+
                 map(context.getCurrentKey(), context.getCurrentValue(), context);
             }
             cleanup(context);
@@ -103,9 +98,9 @@ public class Question5 extends Configured implements Tool
         protected void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException 
         {
             int sum = 0;
-            for (IntWritable val : values) 
+            for (IntWritable v : values) 
             {
-                sum += val.get();
+                sum = sum + v.get();
             }
             context.write(key, new IntWritable(sum));
         }
@@ -114,33 +109,39 @@ public class Question5 extends Configured implements Tool
     public static class Reducer2 extends Reducer<LongWritable, Text, Text, Text> 
     {
         @Override
-        protected void reduce(LongWritable key, Iterable<Text> trends, Context context) throws IOException, InterruptedException 
+        protected void reduce(LongWritable key, Iterable<Text> values, Context context) throws IOException, InterruptedException 
         {
 
-            for (Text val : trends) 
+            for (Text v : values) 
             {
-                context.write(new Text(val.toString()),new Text(key.toString()));
+                context.write(new Text(v.toString()),new Text(key.toString()));
             }
         }
     }
 
-   
 
-
-
-	private static final String inputPath = "/user/hadoop1/tweets";
-	private static final String outputPath ="/user/hadoop1/newtweetout";
-	private static final String tempPath1 = "/user/hadoop1/newtweettemp1";
-    private static final String tempPath2 = "/user/hadoop1/newtweettemp2";
-	
-    @Override
-    public int run(String[] args) throws Exception 
+    public static void main(String[] args) throws Exception 
     {
-        Configuration conf = getConf();
+    	
+        if(args.length!=2)
+        {
+            System.out.println("Provide input and output as command line argument");
+            System.exit(1);
+        }
+
+
+        String inputPath = args[0];
+        String outputPath = args[1];
+        String tempPath1 = args[1]+"TempFile1";
+        String tempPath2 = args[1]+"TempFile2";
+
+        
+        Configuration conf = new Configuration();
         
         Job job1 = new Job(conf);
         job1.setJarByClass(Question5.class);       
-        
+
+
         job1.setOutputKeyClass(Text.class);
         job1.setOutputValueClass(IntWritable.class);
         
@@ -156,70 +157,59 @@ public class Question5 extends Configured implements Tool
         FileInputFormat.setInputPaths(job1, new Path(inputPath));
         FileOutputFormat.setOutputPath(job1, new Path(tempPath1));
 
-        boolean succ = job1.waitForCompletion(true);
-        if (! succ) {
-          System.out.println("Job1 failed, exiting");
-          return -1;
-        }
+
+        job1.waitForCompletion(true);
+    
         
+        ////////////////////////////////////////////////////
+        Job job2 = new Job(conf, "Job2");
+
         
-        //---------------------------------------------
-        
-        Job job2 = new Job(conf, "top-k-pass-2");
-        FileInputFormat.setInputPaths(job2, new Path(tempPath1));
-        FileOutputFormat.setOutputPath(job2, new Path(tempPath2));
+        job2.setMapOutputKeyClass(LongWritable.class);
+        job2.setMapOutputValueClass(Text.class);
        
         job2.setMapperClass(Mapper2.class);
         job2.setReducerClass(Reducer2.class);
         
+
         job2.setInputFormatClass(TextInputFormat.class);
-        job2.setMapOutputKeyClass(LongWritable.class);
-        job2.setMapOutputValueClass(Text.class);
-        job2.setSortComparatorClass(LongWritable.DecreasingComparator.class);
-        job2.setOutputFormatClass(TextOutputFormat.class);
-       // job2.setNumReduceTasks(1);
-        succ = job2.waitForCompletion(true);
-        if (! succ) {
-          System.out.println("Job2 failed, exiting");
-          return -1;
-        }
-
-
-   //---------------------------------------------
+        job2.setOutputFormatClass(TextOutputFormat.class);  
+      
         
-        Job job3 = new Job(conf, "top-k-pass-3");
-        FileInputFormat.setInputPaths(job3, new Path(tempPath2));
-        FileOutputFormat.setOutputPath(job3, new Path(outputPath));
+        job2.setSortComparatorClass(LongWritable.DecreasingComparator.class);        
+       // job2.setNumReduceTasks(1);
+
+
+        FileInputFormat.setInputPaths(job2, new Path(tempPath1));
+        FileOutputFormat.setOutputPath(job2, new Path(tempPath2));
+
+        job2.waitForCompletion(true);
+
+
+        ////////////////////////////////////////////////////
+   
+        
+        Job job3 = new Job(conf, "Job3");
+
+
+        job3.setMapOutputKeyClass(LongWritable.class);
+        job3.setMapOutputValueClass(Text.class);
        
         job3.setMapperClass(Mapper3.class);
         job3.setReducerClass(Reducer2.class);
 
+
         job3.setInputFormatClass(TextInputFormat.class);
-        job3.setMapOutputKeyClass(LongWritable.class);
-        job3.setMapOutputValueClass(Text.class);
-        job3.setSortComparatorClass(LongWritable.DecreasingComparator.class);
         job3.setOutputFormatClass(TextOutputFormat.class);
-       // job3.setNumReduceTasks(1);
-        succ = job3.waitForCompletion(true);
-        if (! succ) {
-          System.out.println("Job3 failed, exiting");
-          return -1;
-        }
 
-        
-        return 0;
-        
-    }
 
-    public static void main(String[] args) throws Exception 
-    {
-    	String path [] = new String [4];
-    	path[0] = inputPath;
-    	path[1] = tempPath1;
-        path[2] = tempPath2;
-        path[3] = outputPath;
-    	
-        int res = ToolRunner.run(new Question5(), path);
-        System.exit(res);
+        job3.setSortComparatorClass(LongWritable.DecreasingComparator.class);
+        // job3.setNumReduceTasks(1);
+
+
+        FileInputFormat.setInputPaths(job3, new Path(tempPath2));
+        FileOutputFormat.setOutputPath(job3, new Path(outputPath));
+       
+        job3.waitForCompletion(true);
     }
 }
